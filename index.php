8bb9a40c438a832ad17855a5f8661826338c53b1
forbalieveme <haner.avella@protonmail.com>
2021-02-14 16:36:08 -0500

    Agrega las clases de implementacion del modelo mvc: modelo, vitsa, controlador

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Plantilla</title>
-</head>
-
-<body>
-    <h1>Hola Mundo</h1>
-    <?php
+<?php
+    // require_once "app/core/Model.php";
+    require_once "app/core/View.php";
+    require_once "app/core/Controller.php";
     require_once "app/core/App.php";
 
-    $recurso = $_REQUEST['url'];
-    var_dump($recurso);
-    echo 'Buscamos en ' . $recurso;
     $app = new App();
-    ?>
-</body>
-</html>
