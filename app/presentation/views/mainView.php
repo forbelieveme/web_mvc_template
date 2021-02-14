4289f766e390b582fc0eba92732b2aeb28ec372b
forbalieveme <haner.avella@protonmail.com>
2021-02-14 16:44:24 -0500

    Crea la vitsa principal que va a estar renderisada por el MainController

---
+++ app/presentation/views/mainView.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Esta es la vista principal</h2>
+</body>
+</html>
