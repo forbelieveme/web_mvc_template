70b7bcc969a5b5f3c24668d5f50c0110d13be690
forbalieveme <haner.avella@protonmail.com>
2021-02-14 19:35:45 -0500

    Inserta los elementos header y footer a la vista mainView

--- app/presentation/views/mainView.php
+++ app/presentation/views/mainView.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Document</title>
-</head>
-<body>
+<?php
+    require 'app/presentation/views/includes/header.php'
+?>
+
+
+
     <h2>Esta es la vista principal</h2>
-</body>
-</html>
+
+
+
+<?php
+    require 'app/presentation/views/includes/footer.php'
+?>
